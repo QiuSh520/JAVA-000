1c18cf30d803d8c9cd5e037791cb905d18ce88fb
QiuShuang <943025703@qq.com>
2020-11-17 22:34:47 +0800

    第十次课第一道必做题

---
+++ Week_05/代码/demo-spring-boot-starter/src/test/java/io/kimmking/springboot01/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kimmking.springboot01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
